5134679193610d42d4762c77846e5259b7b38908
Lans Hung <lans@lansceo.(none)>
2013-12-07 09:15:05 +0000

    change file name for uber.py

--- uber/uber.py
+++ uber/uber.py
@@ -1,4 +1,6 @@
+import sys
 import os
+sys.path.append(os.path.abspath('..'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 from uber.models import TripEvent, UberBase
 
@@ -9,12 +11,17 @@ def record_trip_event(client_id, driver_id, start_time, lat, lng, fare, distance
                   lat=lat, lng=lng, fare=fare, distance=distance, rating=rating)
     e.save()
 
+def delete_all_records():
+    TripEvent.objects.all().delete()
+
+def print_all_records():
+    return TripEvent.objects.all()
+
 if __name__ == "__main__":
-    print "TripEvent.objects.all().delete(): ", TripEvent.objects.all().delete()
+    delete_all_records()
     record_trip_event(1, 11, 1385895912, Decimal("37.774650"), Decimal("-122.419122"), Decimal("5.0"), Decimal("5.0"), 5) #SF
     record_trip_event(1, 12, 1385895926, Decimal("37.790184"), Decimal("-122.429845"), Decimal("3.0"), Decimal("5.0"), 3) #SF
     record_trip_event(2, 11, 1385895927, Decimal("37.68776"),  Decimal("-122.486598"), Decimal("10.0"), Decimal("6.0"), 4) #Daly City
     record_trip_event(3, 12, 1385895928, Decimal("37.637072"), Decimal("-122.418277"), Decimal("11.0"), Decimal("7.0"), 4) # San Bruno
     record_trip_event(4, 12, 1417850394, Decimal("37.5688"),   Decimal("-122.316336"), Decimal("12.0"), Decimal("8.0"), 5) #future #San Mateo
-    print "TripEvent.objects.all(): ", TripEvent.objects.all()
-    
+    print print_all_records()
